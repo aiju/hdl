51a43547bc7844c18e52a5d386804793a6529998
Julius Schmidt <aiju@phicode.de>
2015-12-15 22:19:57 +0100

    add xlnx/fns.tcl

---
+++ xlnx/fns.tcl
@@ -0,0 +1,54 @@
+set thepart xc7z015clg485-1
+set hdl [file normalize [file dirname [info script]]/..]
+
+proc new {} {
+	global thepart
+	close_project -quiet
+	create_project -in_memory -part $thepart
+}
+
+proc here {} {
+	file dirname [info script]
+}
+
+proc checktarget {} {
+	global target
+	
+	if {![info exists target]} {
+		set target [here]/build
+	}
+	file mkdir $target
+}
+
+proc synth {top {flatten_hierarchy none}} {
+	global target thepart
+	checktarget
+
+	synth_design -top $top -part $thepart -flatten_hierarchy $flatten_hierarchy
+	write_checkpoint -force $target/post_synth
+}
+
+proc implement {} {
+	global target
+	checktarget
+	
+	opt_design
+	place_design
+	write_checkpoint -force $target/post_place
+	phys_opt_design
+	route_design
+	write_checkpoint -force $target/post_route
+	
+	report_timing_summary -file $target/post_route_timing.rpt
+	report_utilization -file $target/post_route_util.rpt
+	
+	if {! [string match -nocase {*timing constraints are met*} \
+	[report_timing_summary -no_header -no_detailed_paths -return_string]]} \
+	{puts "timing constraints not met"}
+}
+
+proc bitstream {} {
+	global target
+	checktarget
+	write_bitstream -force -bin_file -file $target/out.bit
+}
