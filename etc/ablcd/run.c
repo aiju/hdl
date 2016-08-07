772fc5fa2302690d8856e4eeced38b2aa66b37d1
Julius Schmidt <aiju@phicode.de>
2016-08-07 18:41:51 +0200

    etc/ablcd: update code

--- etc/ablcd/run.c
+++ etc/ablcd/run.c
@@ -1,18 +1,42 @@
 #include <u.h>
 #include <libc.h>
 
-int
+int *reg;
+
+void
+cmd(char *fmt, ...)
+{
+	va_list va;
+	char *s, *p;
+	
+	va = va_start(va, fmt);
+	s = vsmprint(fmt, va);
+	va_end(va);
+	for(p = s; *p != 0; p++)
+		*reg = *p;
+	free(s);
+}
+
+void
 main()
 {
-	int c;
-	int *v;
+	char c;
 	
-	v = segattach(0, "axi", nil, 4096);
-	if(v == (int*)-1)
+	reg = segattach(0, "axi", nil, 4096);
+	if(reg == (int*)-1)
 		sysfatal("segattach: %r");
+	*reg = 0;
+	cmd("CLTT");
 	for(;;){
 		if(read(0, &c, 1) <= 0)
-			return 0;
-		*v = c;
+			exits(nil);
+		if(c == '\f'){
+			*reg = 0;
+			cmd("CLTT");
+			continue;
+		}
+		if(c == '\n')
+			*reg = 13;
+		*reg = c;
 	}
 }
